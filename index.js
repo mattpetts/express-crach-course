88c823dd256b9d7aeba96956f4d5b61b94d2a91e
mattpetts <mattpetts23@gamil.com>
2021-01-12 21:20:59 +0000

    rendering handlebars views

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const path = require('path');
 const logger = require('./middleware/logger');
-
+const exphbs = require('express-handlebars');
 const app = express();
-
+const members = require('./Members');
 
 // Init middleware
 //app.use(logger)
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
+// Render a handlebars index
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Member App',
+        members
+    })
+});
 
 
-// set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+
+// set static folder for rendering HTML
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/members', require('./routes/api/members'))
 
